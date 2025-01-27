cc3e95eebd88c0347e21f6e3e62102d0627dcdb5
Bhavya Jain <148266515+bhavya7612@users.noreply.github.com>
2025-01-27 19:37:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, render_template, jsonify, session, redirect
+from flask import Flask, request, render_template, jsonify, session, redirect, send_file
+from gtts import gTTS
+from langdetect import detect
+from io import BytesIO
 from sqldb import mysqlconnector
 import summariser
+import translator
 
 app=Flask(__name__)
 app.secret_key='123-456-789'
@@ -55,30 +59,88 @@ def projectpage():
     else:
         return render_template('login.html', message="You are not logged in! Please login first.")
 
+@app.route('/speak', methods=['POST'])
+def text_to_speech():
+    try:
+        # Parse JSON data from the frontend
+        data = request.get_json()
+        text = data.get('text')
+        language = data.get('language')
+        lang_det = detect(text)
+
+        if not text:
+            return jsonify({'error': 'Text is required!'}), 400
+        
+        if lang_det != language:
+            language = lang_det
+
+        # Generate a unique filename for the audio
+        # with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False, dir="/tmp") as temp_file:
+        #     filename = temp_file.name
+
+        # Convert text to speech
+        tts = gTTS(text=text, lang=language, slow=False)
+        audio_file=BytesIO()
+        tts.write_to_fp(audio_file)
+        audio_file.seek(0)
+        # tts.save(filename)
+
+        # Serve the audio file
+        response = send_file(audio_file, mimetype='audio/mpeg', as_attachment=False, download_name='output.mp3')
+
+        return response
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/output',methods=['GET','POST'])
 def summarise():
     if request.method=='POST':
         url=request.form['url']
         max_len=request.form.get('max_len','')
-        lang=request.form['lang']
+        temperature=request.form['temperature']
+        temperature=float(temperature)
         if not max_len.isdigit():
             max_len=150
         else:
             max_len=int(max_len)
-        video_id=url.split('=')[1]
+        if url[11]=='.':
+            video_id = url.split('=')[1]
+        else:
+            video_id = url.split('?')[0][17:]
         title=db_obj.insert_video_info(video_id,session['id'])
-        transcript=summariser.get_transcript(video_id)
-        summary=summariser.summarise(video_id, max_len, lang)
+        result=summariser.summarise(video_id, max_len, temperature)
+        summary=result[0]
+        transcript=result[1]
         langs={
                 'en':'English', 'hi':'Hindi', 'mr':'Marathi',\
                 'gu':'Gujarati', 'ml':'malayalam', 'kn':'Kannada',\
                 'bn':'Bengali', 'pa':'Punjabi', 'ta':'Tamil',\
                 'te':'Telugu', 'ar':'Arabic', 'fr':'French',\
                 'de':'German', 'ja':'Japanese', 'ru':'Russian', 'es':'Spanish'}
-        # tr_len=len(transcript.split())
-        # sum_len=len(summary.split())
-        return render_template('output.html', transcript=transcript, summary=summary, title=title, lang=langs[lang])
+        
+        summary_hi=translator.translate_to_hindi(summary)
+        summary_mr=translator.translate_to_marathi(summary)
+        summary_guj=translator.translate_to_guj(summary)
+        summary_malaya=translator.translate_to_malayalam(summary)
+        summary_kan=translator.translate_to_kannada(summary)
+        summary_ben=translator.translate_to_bengali(summary)
+        summary_pj=translator.translate_to_punjabi(summary)
+        summary_tam=translator.translate_to_tamil(summary)
+        summary_tel=translator.translate_to_telugu(summary)
+        summary_ar=translator.translate_to_arabic(summary)
+        summary_french=translator.translate_to_french(summary)
+        summary_germ=translator.translate_to_german(summary)
+        summary_jap=translator.translate_to_japanese(summary)
+        summary_rus=translator.translate_to_russian(summary)
+        summary_sp=translator.translate_to_spanish(summary)
+        
+        return render_template('output.html', transcript=transcript, vid_title=title, summary_en=summary,\
+                               summary_hi=summary_hi, summary_mr=summary_mr, summary_guj=summary_guj,\
+                               summary_malaya=summary_malaya, summary_kan=summary_kan, summary_ben=summary_ben,\
+                               summary_pj=summary_pj,  summary_tam=summary_tam, summary_tel=summary_tel,\
+                               summary_ar=summary_ar, summary_french=summary_french, summary_germ=summary_germ,\
+                               summary_jap=summary_jap, summary_rus=summary_rus, summary_sp=summary_sp)
     else:
         return render_template('output.html')
 
--- video_info.py
+++ video_info.py
@@ -1,13 +1,41 @@
-from googleapiclient.discovery import build
 import os
+from googleapiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-api_key=os.getenv("YT_API_KEY")
-youtube = build('youtube', 'v3', developerKey=api_key)
+yt_api_key=os.getenv("YT_API_KEY")
+youtube = build('youtube', 'v3', developerKey=yt_api_key)
 
+# get video title through api_key
 def get_video_title(video_id):
-    request=youtube.videos().list(part="snippet", id=video_id)
-    response=request.execute()
-    video_title=response['items'][0]['snippet']['title']
-    return video_title
+    try:
+        request=youtube.videos().list(part="snippet", id=video_id)
+        response=request.execute()
+        video_title=response['items'][0]['snippet']['title']
+        return video_title
+    except Exception as e:
+        print(f"error is {e}")
+        return "Cannot find title"
+
+def get_video_transcript(video_id):                                              
+    proxies={
+        "https":f'{os.getenv("proxy2")}',
+    }
+
+    try:
+        session = requests.Session()
+        session.proxies.update(proxies)
+        transcript_list = YouTubeTranscriptApi.get_transcript(
+            video_id,
+            languages = ['en','en-IN','en-US','en-UK','es','hi','de','fr','ru','ja','ar'],
+            # proxies = session.proxies
+        )
+        transcript_text=""
+        for d in transcript_list:
+            transcript_text += " " + d['text']
+        return transcript_text
+    except Exception as e:
+        print(f"Error is {e}")
+        return "No Transcript Found"
